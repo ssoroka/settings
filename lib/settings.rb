d35fb90e7d5b0bcd1f68c091fa6769ea0b457294
Steven Soroka <ssoroka78@gmail.com>
2013-06-10 13:56:00 -0400

    codeeee

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,5 +1,40 @@
 require "settings/version"
+require 'active_support/hash_with_indifferent_access'
 
-module Settings
-  # Your code goes here...
+class Settings
+  class ESettingsMissing < Exception; end
+  class SettingsHash < ::HashWithIndifferentAccess
+    def [](setting)
+      result = super(setting.to_s)
+      case result
+      when Hash, nil
+        SettingsHash.new(result)
+      else
+        result
+      end
+    end
+
+    def []=(k, value)
+      super(k.to_s, value)
+    end
+  end
+
+  def self.[](setting)
+    all[setting]
+  end
+
+  def self.all
+    @all ||= SettingsHash.new(create_or_read_settings_file[Rails.env])
+  end
+
+  def self.settings_file
+    File.expand_path('config/settings.yml')
+  end
+
+  def self.create_or_read_settings_file
+    if !File.exist?(settings_file)
+      raise ESettingsMissing, "Settings file #{settings_file} missing; example contents should be:\n#{{"development" => {'your_settings_here' => 'ok?'}}.to_yaml}"
+    end
+    YAML.load(File.read(settings_file))
+  end
 end
